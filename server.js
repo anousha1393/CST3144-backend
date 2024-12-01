fa99913e306e8ba9bc237cb073cc32dc7a1ccb4c
anousha1393 <atariq1393@gmail.com>
2024-12-01 19:48:06 +0400

    Update server.js
    This file serves as the main entry point for the backend server of the application.

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use((req, res, next) => {
 // Configure bodyPaser to parse incoming JSON data
 app.use(bodyParser.json());
 
+// Serve files from the 'public' directory
+app.use(express.static('public'));
 // Serve files from the 'images' directory
 app.use(express.static('images'));
 
